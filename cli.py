438a6ac868ba9fbff85386f0ce170b3cbdaed88b
Roshani Jha <roshanigjha@gmail.com>
2026-04-30 17:40:19 +0530

    Added menu system and order history feature

--- cli.py
+++ cli.py
@@ -1,19 +1,34 @@
 from bot.utils import *
 from bot.orders import place_order
-
 while True :
-    print("\n---New Order---")
-    price = None
-    symbol = input("Enter symbol : ").upper()
-    side = get_side()
-    order_type = get_order_type()
-    if order_type == "LIMIT" :
-        price = get_price()
-    quantity = get_quantity()
+    print("\n===== Trading Bot =====")
+    print("1. Place Order")
+    print("2. View Order History")
+    print("3. Exit")
+    choice = input("Enter your choice : ")
+    if choice == "1" :
+        print("\n---New Order---")
+        price = None
+        symbol = input("Enter symbol : ").upper()
+        side = get_side()
+        order_type = get_order_type()
+        if order_type == "LIMIT" :
+            price = get_price()
+        quantity = get_quantity()
+        place_order(side,order_type,symbol,quantity,price)
 
-    place_order(side,order_type,symbol,quantity,price)
+    elif choice == "2" :
+        print("\n---Order History---")
+        try :
+            with open("orders.log","r") as file :
+                for line in file :
+                    print(line.strip())
+        except FileNotFoundError :
+            print("No orders found yet.")
 
-    again = input("Do you want to place another order(yes/no) : ").lower()
-    if again != "yes" :
-        print("Exiting trading bot...")
+    elif choice == "3" :
+        print("Exiting Trade Bot...")
         break
+
+    else :
+        print("Invalid Input. Please enter 1, 2 or 3")
